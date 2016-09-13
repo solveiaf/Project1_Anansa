3492d872e1dd6307c70ccfa6938c07c227085465
solveiaf <solveiaf@student.matnat.uio.no>
2016-09-13 16:49:20 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,30 +1,31 @@
-#include <QCoreApplication>
-#include <cmath>
+//#include <QCoreApplication>
+//#include <cmath>
 #include <iostream>
 using namespace std;
 
 int main()
 {
     //Defining variables
-    int m,k,j, i, n;
+    int m,k,j, n;
     cout << "Dimension of matrix:";
     cin >> n;
 
     double matr[n][n], vec[n];
 
 
-    for (k = 0; k < n; k++) {
-        vec[k] = k;
+    for (k = 0; k <= n; k++) {
+        vec[k] = 1;
         // fill in matr
-            for (m = 0; m < n; m++){
-                for (k = 0; k < n ; k++){
-                    matr[m][k] = m + 10*k;
-    }}}
+        matr[k][k] = 1;
+        //cout << matr[k][k] << endl;
+    }
+
+    //}
     // wri
     //Algorithm forward substitution
-    for (m = 0; m < n; m++){
-        for (j = m+1; j < n; j++){
-            for (k = j; k < n; k++){
+    for (m = 0; m <= n; m++){
+        for (j = m+1; j <= n; j++){
+            for (k = j; k <= n; k++){
 
                 //a^(m+1)_jk = a^m_jk - (a^m_jm * a^m_mk)/a^m_mm
                 matr[j][k] = matr[j][k] - (matr[j][m]*matr[m][k])/matr[m][m];
@@ -37,23 +38,25 @@ int main()
     }
 
     //Defining variables for backward substitution
-    double x[n], sum;
+    double x[n], sum,term;
     //Defining the first solution
-    x[-1] = matr[n][n];
+    x[n] = matr[n][n];
 
     //Algorithm for backward subtitution
     for (m = n - 1; m >= 0; m--){
         for (k=m+1; k<=n; k++){
 
+            //cout << matr[m][k] << endl;
             //SUM_(k+1)^n (b_mk*x_k)
-            sum += matr[m][k]*x[k];
-
+            term = matr[m][k]*x[k];
+            sum += term;
+            //cout << sum <<endl;
         }
         // x_m = (1/b_mm)*(y_m - SUM
         x[m]= (1.0/matr[m][m])*(vec[m]-sum);
     }
     cout <<  "Content of vector in subfunction:" << endl;
-    for (m = 0; m < n; m++){
+    for (m = 0; m <= n; m++){
     cout << x[m]<<endl;
     }
     return 0;
